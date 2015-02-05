27529af9904f8cfb550f1acdbe28cc2f84cd8dcf
yc09bc2 <bchalich@optaros.com>
2015-02-05 11:48:09 -0500

    added tests and updated package.js

--- package.js
+++ package.js
@@ -1,12 +1,37 @@
+
+
+// ---------------------------------------------
+// Description
+
 Package.describe({
-	summary: 'A node.js driver for mssql, extended to Meteor.'
+	name: 'elosoman:meteor-mssql',
+	version: '0.0.1',
+	summary: 'A node.js driver for mssql, extended to Meteor.',
+	git: 'https://github.com/elosoman/meteor-mssql',
+	documentation: 'README.md'
 });
 
-Package.on_use(function(api) {
+
+// ---------------------------------------------
+// Use
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0.3.1');
+
 	api.add_files(['lib/mssql.js'], 'server');
 	if (typeof api.export !== 'undefined') { 
 		api.export('Mssql', 'server'); 
 	}
 });
 
-Npm.depends({ 'mssql': '1.3.0' });
+
+// ---------------------------------------------
+// Tests
+Package.onTest(function(api) {
+	api.use('tinytest');
+	api.use('meteor-mssql');
+
+	api.addFiles('meteor-mssql-tests.js');
+});
+
+
